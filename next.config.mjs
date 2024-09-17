829f7bf08db745595999419ed7eed7f6516cf9d2
Petya <sk.petya02@mail.ru>
2024-09-17 16:35:05 +0500

    gh-pages settings

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  images: {
+      unoptimized: true,
+  },
+  basePath: '/about-me',
+  assetPrefix: '/about-me',
 };
 
 export default nextConfig;
